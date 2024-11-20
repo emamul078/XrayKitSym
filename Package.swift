0deaeac9e4ec6cbbef0aedd0647f09c9f40fa50e
emamul078 <86395892+emamul078@users.noreply.github.com>
2024-11-20 14:43:01 +0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,15 +2,15 @@
 import PackageDescription
 
 let package = Package(
-  name: "XrayKitSym",
+  name: "XrayKit",
   platforms: [.iOS(.v12), .macOS(.v12)],
   products: [
-    .library(name: "XrayKitSym", targets: ["XrayKitSym"])
+    .library(name: "XrayKit", targets: ["XrayKit"])
   ],
   targets: [
     .binaryTarget(
-      name: "XrayKitSym",
-      url: "https://github.com/emamul078/XrayKitSym/releases/download/0.1.2/XrayKit.xcframework.zip",
+      name: "XrayKit",
+      url: "https://github.com/ghostwolf90/XrayKit/releases/download/0.1.45/XrayKit.xcframework.zip",
       checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
