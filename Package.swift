aab1182fbac3511f380049fa8e06ed73ac70ae8c
emamul078 <86395892+emamul078@users.noreply.github.com>
2024-11-20 12:56:01 +0600

    changedURLPath

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "XrayKit",
-      url: "https://github.com/emamul078/XrayKitSym/raw/refs/heads/main/XrayKit.xcframework.zip",
+      url: "https://github.com/emamul078/XrayKitSym/releases/download/0.1.2/XrayKit.xcframework.zip",
       checksum: "541f9bf3f1b79f5c9f88899eab5f47c021ef7f0aa52c4373d9cb9cb236407afc"
     )
   ]
